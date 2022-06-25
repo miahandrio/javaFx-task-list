ce614b540298aeaef80a9a17500c933af6173000
Mykhailo Bubnov <applemix00@gmail.com>
2022-06-25 16:39:04 +0200

    Functional added

--- src/main/java/com/example/javafxtasklist/HelloController.java
+++ src/main/java/com/example/javafxtasklist/HelloController.java
@@ -1,14 +1,35 @@
 package com.example.javafxtasklist;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ListView<String> listView;
+
+    @FXML
+    private TextField textField;
+
+    @FXML
+    protected void onAddButtonClick() {
+        if (textField.getText().length() > 0) {
+            listView.getItems().add(textField.getText());
+            textField.clear();
+        }
     }
+
+    @FXML
+    protected void onTextFieldClick() {
+        System.out.println("worked");
+//        listView.
+    }
+
+    @FXML
+    protected void exit() {
+        Platform.exit();
+    }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.javafxtasklist {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires java.logging;
 
 
     opens com.example.javafxtasklist to javafx.fxml;
